303a2897860b1544001b43d76275224fb59bc1da
deciduously <bendlovy@gmail.com>
2018-09-03 20:25:04 -0400

    apply standard

--- www/index.js
+++ www/index.js
@@ -1,76 +1,77 @@
-import { Game } from "dots";
+/* jshint esversion: 6 */
+import { Game } from 'dots'
 
 // capping the tick to 60 times a second
-const updates_per_second = 60;
-const millis_per_update = 1 / updates_per_second * 1000;
+const updatesPerSecond = 60
+const millisPerUpdate = 1 / updatesPerSecond * 1000
 
 // start things up - Game is our WASM interface
-const game = Game.new();
+const game = Game.new()
 
 // set up the render context
-const canvas  = document.getElementById("dots-canvas");
-const height = game.height();
-const width = game.width();
-canvas.height = height;
-canvas.width = width;
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementById('dots-canvas')
+const height = game.height()
+const width = game.width()
+canvas.height = height
+canvas.width = width
+const ctx = canvas.getContext('2d')
 ctx.globalAlpha = 0.8 // everything's a little transparent
 
 // Restart button
-const restartButton = document.getElementById("restart-button");
-restartButton.addEventListener("click", event => {
-    game.restart_game();
-});
+const restartButton = document.getElementById('restart-button')
+restartButton.addEventListener('click', event => {
+  game.restart_game()
+})
 
 // Canvas click handler
-canvas.addEventListener("click", event => {
-    // translate from page coords to canvas coords
-    // shamelessly lifted from the RustWasm book
-    // https://rustwasm.github.io/book/game-of-life/interactivity.html
-    const boundingRect = canvas.getBoundingClientRect();
+canvas.addEventListener('click', event => {
+  // translate from page coords to canvas coords
+  // shamelessly lifted from the RustWasm book
+  // https://rustwasm.github.io/book/game-of-life/interactivity.html
+  const boundingRect = canvas.getBoundingClientRect()
 
-    const scaleX = canvas.width / boundingRect.width;
-    const scaleY = canvas.height / boundingRect.height;
-  
-    const canvasX = (event.clientX - boundingRect.left) * scaleX;
-    const canvasY = (event.clientY - boundingRect.top) * scaleY;
+  const scaleX = canvas.width / boundingRect.width
+  const scaleY = canvas.height / boundingRect.height
 
-    game.add_player(canvasX, canvasY);
-});
+  const canvasX = (event.clientX - boundingRect.left) * scaleX
+  const canvasY = (event.clientY - boundingRect.top) * scaleY
+
+  game.add_player(canvasX, canvasY)
+})
 
 // define the main loop, updated 60 times per second
 const renderLoop = () => {
-    if (Date.now() - game.last_update() >= millis_per_update) {
-        game.tick()
-        drawGame();
-        requestAnimationFrame(renderLoop);
-    }
+  if (Date.now() - game.last_update() >= millisPerUpdate) {
+    game.tick()
+    drawGame()
+    window.requestAnimationFrame(renderLoop)
+  }
 }
 
 // Define how to draw a single frame
 const drawGame = () => {
-    const numDots = game.num_dots();
-    
-    // Start with a blank slate
-    ctx.clearRect(0, 0, width, height);
+  const numDots = game.num_dots()
 
-    // Draw the progress counter
-    ctx.font = '32px serif';
-    ctx.fillStyle = 'red';
-    ctx.fillText(game.get_progress_text(), 10, 42);
+  // Start with a blank slate
+  ctx.clearRect(0, 0, width, height)
 
-    // draw each dot, grabbing params from the WASM
-    for (let idx = 0; idx < numDots; idx++) {
-        if (game.draw_dot(idx)) {
-            ctx.beginPath();
-            // use an arc from 0 to 2pi to draw a full circle
-            ctx.arc(game.get_dot_x(idx), game.get_dot_y(idx), game.get_dot_radius(idx), 0, 2 * Math.PI, false);
-            ctx.fillStyle = game.get_dot_color(idx);
-            ctx.fill();
-            ctx.stroke();
-        }
+  // Draw the progress counter
+  ctx.font = '30px serif'
+  ctx.fillStyle = 'red'
+  ctx.fillText(game.get_progress_text(), 10, 42)
+
+  // draw each dot, grabbing params from the WASM
+  for (let idx = 0; idx < numDots; idx++) {
+    if (game.draw_dot(idx)) {
+      ctx.beginPath()
+      // use an arc from 0 to 2pi to draw a full circle
+      ctx.arc(game.get_dot_x(idx), game.get_dot_y(idx), game.get_dot_radius(idx), 0, 2 * Math.PI, false)
+      ctx.fillStyle = game.get_dot_color(idx)
+      ctx.fill()
+      ctx.stroke()
     }
+  }
 }
 
 // Kick off the render loop by asking for the first frame
-requestAnimationFrame(renderLoop);
+window.requestAnimationFrame(renderLoop)
